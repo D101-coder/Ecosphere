2e68130df43afa4f629590bd5803dc110d58964c
D101-coder <dhwanidalwadi0123@gmail.com>
2026-07-12 15:30:20 +0530

    Create main App component with routing

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import { AuthProvider } from "./contexts/AuthContext";
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard/*" element={<Dashboard />} />
+      </Routes>
+    </AuthProvider>
+  );
+}
